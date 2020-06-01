dc388ee470d8dbf4082217684584606ef07c98eb
vhetet <vh1030478@gmail.com>
2020-05-31 20:58:15 -0500

    add d3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef, useEffect, useState } from 'react';
+import { select } from "d3";
 import './App.css';
 
+
 function App() {
+  const [data, setData] = useState([34, 54, 15, 78, 90])
+  const svgRef = useRef()
+
+  useEffect(() => {
+    const svg = select(svgRef.current)
+
+    svg
+      .selectAll("circle")
+      .data(data)
+      .join("circle")
+      .attr("r", value => value)
+      .attr("cx", value => value * 2)
+      .attr("cy", value => value * 2)
+      .attr("stroke", "red")
+  }, [data])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <svg ref={svgRef}></svg>
+      <button onClick={() => setData(data.map(value => value + 5))}>
+        Update data
+      </button>
+      <button onClick={() => setData(data.filter(value => value > 35))}>
+        Filter data
+      </button>
+    </React.Fragment>
   );
 }
 
